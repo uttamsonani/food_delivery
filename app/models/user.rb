f10e57ef2ada4ab99279c2770759939f945d7d55
Uttam Sonani <uttamsoftices@gmail.com>
2022-05-17 11:16:30 +0530

    Adding image link

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@ class User < ApplicationRecord
     has_many :orders
 
 # VALIDATION
-    validates :first_name, presence: true
-    validates :first_name, presence: true
+    validates :first_name, :last_name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "Only latters are allowed" }
     validates :email, presence: true, uniqueness: true, format: { with: /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i }
     validates :phone_no, presence: true, numericality: { only_integer: true }
     validates :gender, presence: true
