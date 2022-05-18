27ce75cdb4c73fcfee399e5021165676300a4a0a
Uttam Sonani <uttamsoftices@gmail.com>
2022-05-18 17:26:16 +0530

    adding fonts and icon

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,17 +2,3 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
-
-//= link 'jquery-3.2.1.min'
-//= link 'animsition.min'
-//= link 'popper'
-//= link 'bootstrap.min'
-//= link 'select2.min'
-//= link 'moment.min'
-//= link 'daterangepicker'
-//= link 'slick.min'
-//= link 'slick-custom'
-//= link 'parallax100'
-//= link 'countdowntime'
-//= link 'lightbox.min'
-//= link 'main'
