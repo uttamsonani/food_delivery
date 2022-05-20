99d9434e6fc85a4b72f6e2794c23b322bebeddc7
Uttam Sonani <uttamsoftices@gmail.com>
2022-05-20 11:23:33 +0530

    convert home contoller file HTML to HAML

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: "home#index"
+
+  get 'home/menu'
+  get 'home/blog_detail'
+  get 'home/blog'
+  get 'home/contact'
+  get 'home/gallery'
+  get 'home/about'
+  get 'home/index'
+  get 'home/reservation'
+  
   # Defines the root path route ("/")
   # root "articles#index"
 end
