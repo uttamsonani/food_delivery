38f58bcae48ea5ebed4dbac5e248091055289e95
Uttam Sonani <uttamsoftices@gmail.com>
2022-05-23 12:09:31 +0530

    Some changes in route file

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: "home#index"
 
-  get 'home/menu'
-  get 'home/blog_detail'
-  get 'home/blog'
-  get 'home/contact'
-  get 'home/gallery'
-  get 'home/about'
-  get 'home/index'
-  get 'home/reservation'
+  get 'home/menu', as: :menu
+  get 'home/blog_detail', as: :blog_detail
+  get 'home/blog', as: :blog
+  get 'home/contact', as: :contact
+  get 'home/gallery', as: :gallery
+  get 'home/about', as: :about
+  get 'home/index', as: :index
+  get 'home/reservation', as: :reservation
   
   # Defines the root path route ("/")
   # root "articles#index"
