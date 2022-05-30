025d0d0cff82ffbc2ec1c8866f7bfe396f2c522e
Uttam Sonani <uttamsoftices@gmail.com>
2022-05-30 11:41:56 +0530

    Some changes in Pagination and Devise Gem

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-    before_action :authenticate_user!, only: %i[blog_detail, search, menu, reservation]
+    before_action :authenticate_user!, only: %i[search  menu reservation blog_detail]
     
     def index
     end
@@ -26,17 +26,17 @@ class HomeController < ApplicationController
     end
 
     def search
-        @pagy, @foods = pagy(Food.includes(:categories).where("foods.name LIKE ?", "%#{params[:search]}%").order(:name))
-
-        # @foods = Food.where( { id: params[:pr     ice_id] }).order(:price) if params[:price_id].present?
+        @pagy, @foods = pagy(Food.includes(:categories).where("foods.name LIKE ?", "%#{params[:search]}%"))
 
         if params[:category_id].present?
             @foods = @foods.where(categories: { id: params[:category_id] })
         end
-        # if params[:price_id] == "High to Low"
-        #     @foods = @foods.order(:price)
-        # else    
-        #     @foods = @foods.order(price: :desc)
-        # end
+        if params[:price_id] == "High to Low"
+            @foods = @foods.order(price: :desc)
+        elsif params[:price_id] == "Low to High"  
+            @foods = @foods.order(:price)
+        else
+            @foods = @foods.order(:name)
+        end
     end
 end
