e39022d6528e67802af6cb1d65c210f1335a2c1d
Piyush Vaghasiya <piyushvaghasiya2906@gmail.com>
2022-06-14 15:23:47 +0530

    reverted comment

--- app/jobs/reservation_job.rb
+++ app/jobs/reservation_job.rb
@@ -1,11 +1,11 @@
-# class ReservationJob < ApplicationJob
-#   queue_as :default
+class ReservationJob < ApplicationJob
+  queue_as :default
 
-#   retry_on ErrorLoadingSite, wait: 5.minutes, queue: :low_priority 
+  retry_on ErrorLoadingSite, wait: 5.minutes, queue: :low_priority 
   
-#   sidekiq_options retry: 5
+  sidekiq_options retry: 5
   
-#   def perform(*args)
-#     # Do something later
-#   end
-# end
+  def perform(*args)
+    # Do something later
+  end
+end
